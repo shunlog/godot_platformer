c198353ef9f33105cd4feb06968b20cb5f457190
shunlog <artiombalan331@gmail.com>
2023-05-30 23:09:02 +0300

    test tilemap

--- godot/player/player.gd
+++ godot/player/player.gd
@@ -1,5 +1,44 @@
 extends CharacterBody2D
 
+@export var tilemap : TileMap
+var reachable_distance := 10
+
+
+func _surrounding_tiles(cell):
+	var neighb = []
+	for c in tilemap.get_surrounding_cells(cell):
+		if tilemap.get_cell_source_id(1, c) != -1:
+			neighb.append(c)
+	return neighb
+
+func _cells_to_update(cell):
+	var a1 = _surrounding_tiles(cell)
+	var a2 = []
+	for c in a1:
+		a2 += _surrounding_tiles(c)
+		
+	print(cell, a2)
+	return a2
+
+func _tile_clicked(pos):
+	if not tilemap:
+		return
+		
+	var tpos = tilemap.local_to_map(pos)
+	var tself = tilemap.local_to_map(position)
+	if abs(tpos.x - tself.x) + abs(tpos.y - tself.y) > reachable_distance:
+		return
+		
+	tilemap.set_cell(1, tpos, 4, Vector2(2, 0))
+	tilemap.set_cells_terrain_connect(1, tilemap.get_used_cells(1), 0, 3, false)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var pos = get_global_mouse_position()
+			_tile_clicked(pos)
+
+
 # BASIC MOVEMENT VARAIABLES ---------------- #
 var face_direction := 1
 var x_dir := 1

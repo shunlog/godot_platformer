7aea548491e25b4e26ed00bb613cebd13b790d4d
shunlog <artiombalan331@gmail.com>
2023-08-14 12:57:24 +0300

    make player slower

--- godot/player/player.gd
+++ godot/player/player.gd
@@ -58,21 +58,21 @@ func _input(event):
 var face_direction := 1
 var x_dir := 1
 
-@export var max_speed: float = 1300
-@export var acceleration: float = 5500
-@export var turning_acceleration : float = 9600
-@export var deceleration: float = 6500
+@export var max_speed: float = 325
+@export var acceleration: float = 1375
+@export var turning_acceleration : float = 2400
+@export var deceleration: float = 1625
 # ------------------------------------------ #
 
 # GRAVITY ----- #
-@export var gravity_acceleration : float = 5000
-@export var gravity_max : float = 6000
+@export var gravity_acceleration : float = 1500
+@export var gravity_max : float = 2000
 # ------------- #
 
 # JUMP VARAIABLES ------------------- #
-@export var jump_force : float = 2000
+@export var jump_force : float = 700
 @export var jump_cut : float = 0.4
-@export var jump_gravity_max : float = 1800
+@export var jump_gravity_max : float = 450
 @export var jump_hang_treshold : float = 1.0
 @export var jump_hang_gravity_mult : float = 0.1
 # Timers
